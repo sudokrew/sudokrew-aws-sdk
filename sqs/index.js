db2c286b26543f3757f008c6cf6757cb1771e03b
Andrea Takamiya <takamiya.andrea@gmail.com>
2018-12-02 15:06:10 -0800

    Updating sqs files from consumer/producer repos

--- sqs/index.js
+++ sqs/index.js
@@ -1,3 +1,11 @@
 import sqs from './sqs';
+import AWS from 'aws-sdk';
 
-module.exports = sqs;
+// AWS.config.update({endpoint: 'http://localstack', region:process.env.AWS_REGION});
+
+aws.config.update(opts);
+
+module.exports = {
+  AWS,
+  sqs,
+};
