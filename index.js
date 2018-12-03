9963eaaeb7174d0df699c044e8dcdf4c70ad49cc
Andrea Takamiya <takamiya.andrea@gmail.com>
2018-12-02 16:20:08 -0800

    Updating index to include aws-sdk

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 // import sqs from './sqs';
 const sqs = require('./sqs');
 const s3 = require('./s3');
-const logger = require('./services')
+const logger = require('./services');
+const aws = require('aws-sdk');
 
 module.exports = {
+  aws,
   sqs,
   s3,
   logger
 }
-
-
