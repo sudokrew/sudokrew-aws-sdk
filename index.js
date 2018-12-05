df25a38ea863d8877e76027f4f09eadbe61b312f
Andrea Takamiya <takamiya.andrea@gmail.com>
2018-12-04 18:57:21 -0800

    Removing the logger export

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 const sqs = require('./sqs');
 const s3 = require('./s3');
-const logger = require('./services');
 const aws = require('aws-sdk');
 
 module.exports = {
   aws,
   sqs,
-  s3,
-  logger
+  s3
 }
