d0a20e617363059f8f5c51d1be09af53ced4583b
Andrea Takamiya <takamiya.andrea@gmail.com>
2018-12-04 20:20:31 -0800

    Updated catches to use the errorHandler service, updated all instances of logger.info to logger.debug

--- sqs/sqs.js
+++ sqs/sqs.js
@@ -1,5 +1,6 @@
 const { SQS } = require('aws-sdk');
 const { logger } = require('../services');
+const errorHandler = require('../services/errorHandler');
 
 if (!process.env.AWS_SQS_ENDPOINT) {
   throw new Error('AWS_SQS_ENDPOINT environment variable missing');
@@ -15,17 +16,11 @@ function getQueueUrl(queueName) {
 
   const result = sqs.getQueueUrl(params).promise()
     .then(data => {
-      logger.info(`Queue URL retrieved: ${data.QueueUrl}`);
+      logger.debug(`Queue URL retrieved: ${data.QueueUrl}`);
       return data.QueueUrl;
     })
     .catch(err => {
-      if (err.code == 'AWS.SimpleQueueService.NonExistentQueue') {
-        logger.info(`Queue: ${queueName} doesn't exist.`);
-      }
-      else {
-        logger.error(`Unknown error retrieving queue URL. Stack trace: ${err}`);
-        throw (new Error(err));
-      }
+      errorHandler.getSqsError(err);
     });
 
   return result;
@@ -35,21 +30,11 @@ function createQueue(params) {
 
   const result = sqs.createQueue(params).promise()
     .then(data => {
-      logger.info(`Queue created: ${data.QueueUrl}`);
+      logger.debug(`Queue created: ${data.QueueUrl}`);
       return data.QueueUrl;
     })
     .catch(err => {
-      if (err.code == 'QueueAlreadyExists') {
-        logger.info(`Queue: ${queueName} already exists.`);
-      }
-      else if (err.code == 'AWS.SimpleQueueService.QueueDeletedRecently') {
-        logger.error(`Queue: ${queueName} deleted recently`);
-        throw (new Error(err));
-      }
-      else {
-        logger.error(`Unknown error creating queue. Stack trace: ${err}`);
-        throw (new Error(err));
-      }
+      errorHandler.getSqsError(err);
     });
 
   return result;
@@ -60,7 +45,6 @@ function receiveMessage(params) {
 
   const result = sqs.receiveMessage(params).promise()
     .then(data => {
-      logger.info(`Message(s) successfully received from ${params.QueueUrl}`);
       logger.debug(`Message received from queue is: ${JSON.stringify(data)}`);
       if (data.Messages) {
         return data.Messages;
@@ -68,13 +52,7 @@ function receiveMessage(params) {
       return [];
     })
     .catch(err => {
-      if (err.code == 'OverLimit') {
-        logger.error(`OverLimit error received. Params: ${params}. Stack trace: ${err, err.stack}`);
-      }
-      else {
-        logger.error(`Unknown error receiving message. Stack trace: ${err}`);
-        throw (new Error(err));
-      }
+      errorHandler.getSqsError(err);
     });
 
   return result;
@@ -84,20 +62,11 @@ function receiveMessage(params) {
 function deleteMessage(params) {
   const result = sqs.deleteMessage(params).promise()
     .then(data => {
-      logger.info(`Message ${params.ReceiptHandle} successfully deleted from ${params.QueueUrl}`);
+      logger.debug(`Message ${params.ReceiptHandle} successfully deleted from ${params.QueueUrl}`);
       return data.RequestId;
     })
     .catch(err => {
-      if (err.code == 'InvalidIdFormat') {
-        logger.error(`Invalid ID format for deleted message. Params: ${params}. Stack trace: ${err, err.stack}`);
-      }
-      else if (err.code == 'ReceiptHandleIsInvalid') {
-        logger.error(`Receipt handle invalid for deleted message. Params: ${params}. Stack trace: ${err, err.stack}`);
-      }
-      else {
-        logger.error(`Unknown error deleting queue message. Stack trace: ${err}`);
-        throw (new Error(err));
-      }
+      errorHandler.getSqsError(err);
     });
 
   return result;
@@ -106,11 +75,11 @@ function deleteMessage(params) {
 function sendMessage(params) {
   const result = sqs.sendMessage(params).promise()
     .then(data => {
-      logger.info(`Message successfully delivered to ${params.QueueUrl}`);
+      logger.debug(`Message successfully delivered to ${params.QueueUrl}`);
       return data.QueueUrl;
     })
     .catch(err => {
-      logger.error(`Error delivering message. Params: ${params}. Stack trace: ${err, err.stack}`);
+      errorHandler.getSqsError(err);
     });
 
   return result;
