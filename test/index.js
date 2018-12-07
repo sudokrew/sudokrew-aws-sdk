6287c848f1379f3f2a1f0d2e92a06d11668858fe
Andrea Takamiya <takamiya.andrea@gmail.com>
2018-12-07 09:33:10 -0800

    Set up simple mocha chai structure

---
+++ test/index.js
@@ -0,0 +1,21 @@
+const chai = require('chai');
+chai.should();
+
+const s3Service = require('../s3');
+const ssService = require('../sqs');
+
+describe('Connect to s3', () => {
+
+  console.log('test');
+
+
+})
+
+// beforeEach((setup) => {
+//   response = null;
+//   agent.get('/api/counters')
+//     .expect(200)
+//     .expect('Content-Type', /json/)
+//     .expect(res => response = res)
+//     .end(setup);
+// });
