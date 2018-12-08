15e3fb94a782623239be378a79f27bfe50dea84c
Andrea Takamiya <takamiya.andrea@gmail.com>
2018-12-08 15:21:20 -0800

    Map over functions and checking if promises

--- test/index.js
+++ test/index.js
@@ -1,20 +1,29 @@
+const chaiAsPromised = require("chai-as-promised");
 const chai = require('chai');
+const expect = chai.expect;
+
+chai.use(chaiAsPromised);
+
 chai.should();
 
 const s3Service = require('../s3');
-const ssService = require('../sqs');
-describe('Connect to s3', () => {
+const sqsService = require('../sqs');
 
-  console.log('test');
-  console.log('watch me now')
+describe('Check S3 for valid functions', () => {
+  var newObject = Object.keys(s3Service).map(function (key) {
+    it(`should be a function`, () => {
+      ((typeof (s3Service[key])).should.equals('function'))
+    })
 
-})
+    it('rejects as promised', async () => {
 
-// beforeEach((setup) => {
-//   response = null;
-//   agent.get('/api/counters')
-//     .expect(200)
-//     .expect('Content-Type', /json/)
-//     .expect(res => response = res)
-//     .end(setup);
-// });
+      //trying to check that the Error always contains a name, message, and stack
+      await (Promise.resolve(s3Service[key])).reject(new Error()).should.be.rejectedWith(Error);
+      // .that.deep.equals({
+      // 'firstProp': 'foo',
+      // 'secondProp': 'bar',
+      // });
+
+    });
+  });
+});
