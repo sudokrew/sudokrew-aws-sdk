3430b02cee68204682536e8a7bc2a353e8a5e442
jaywon <jason@sudokrew.com>
2018-12-28 09:13:05 -1000

    Add ErrorTypes export for error reference in consuming projects

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const AWS = require('aws-sdk');
 const sqs = require('./sqs');
 const s3 = require('./s3');
 const cloudwatch = require('./cloudwatch');
+const ErrorTypes = require('./services/errorHandler')
 
 module.exports = {
   AWS,
   sqs,
   s3,
-  cloudwatch
+  cloudwatch,
+  ErrorTypes
 }
