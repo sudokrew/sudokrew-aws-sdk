de5634995d0c1d87383b374cb87463d8774e316c
Nick Gambino <ngambino0192@gmail.com>
2019-07-21 12:26:27 -1000

    add putMetricAlarm and setAlarmState functions

--- cloudwatch/cloudwatch.js
+++ cloudwatch/cloudwatch.js
@@ -12,7 +12,7 @@ class CloudwatchWrapper {
     
     const result = this.cloudwatch.putMetricData(metricObj).promise()
       .then(data => {
-        logger.debug(`Successful message sent to CloudWatch ${metricObj}`);
+        logger.debug(`Successful message ${data} sent to CloudWatch: ${JSON.stringify(metricObj)}`);
         return;
       })
       .catch(err => {
@@ -22,7 +22,20 @@ class CloudwatchWrapper {
     return result;
   }
 
-  sendCustomCountMetric(namespace, metricName, value){
+  putMetricAlarm(alarmParams) {
+    
+    const result = this.cloudwatch.putMetricAlarm.promise()
+      .then(data => {
+        logger.debug(`Successful alarm ${data} send to CloudWatch: ${JSON.stringify(alarmParams)}`)
+      })
+      .catch(err => {
+        throw new CommonError(err)
+      })
+    
+    return result;
+  }
+
+  sendCustomCountMetric(namespace, metricName, value) {
 
     const customMetricObj = {
       Namespace: namespace,
@@ -36,6 +49,19 @@ class CloudwatchWrapper {
     }
 
     return this.putMetricData(customMetricObj);
+  }
+
+  setAlarmState(alarmState) {
+
+    const result = this.cloudwatch.setAlarmState.promise()
+      .then(data => {
+      logger.debug(`Sucessful alarm ${data} set in CloudWatch: ${JSON.stringify(alarmState)}`)
+      })
+      .catch(err => {
+      throw new CommonError(err)
+      })
+    
+    return result;
   }
 }
 
